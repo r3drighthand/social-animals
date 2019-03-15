2ccde77a9dfa116fcc80b982cd854855d0ccc1e9
r3drighthand <ryanmccool13@gmail.com>
2019-03-15 18:46:34 -0500

    Add views for UserSessions & modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users 
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
