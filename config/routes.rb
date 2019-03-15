d3566650e32484181fa8615d2fee38e7ad413727
r3drighthand <ryanmccool13@gmail.com>
2019-03-15 18:30:06 -0500

    Add code to UserSessionsCtrl

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
