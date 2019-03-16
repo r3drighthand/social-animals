1c06a449ed0e01a89ff07542dc68d4ecf0e4d1d8
r3drighthand <ryanmccool13@gmail.com>
2019-03-15 19:10:07 -0500

    Add private method to AppCtrl, Add methods to SessionsCtrl & UsersCtrl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:index, :new, :create]
+
   def index
+    # 
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to(:users, notice: 'User was successfully created')
+    else
+      render 'new'
+    end
   end
 
   private
-
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
