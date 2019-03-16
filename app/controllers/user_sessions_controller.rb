c32a7303f3d4a4bcfef7ef46c8a624a319d5f1d1
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2019-03-15 23:49:28 -0500

    Update app/controllers/user_sessions_controller.rb
    Co-Authored-By: r3drighthand <ryanmccool13@gmail.com>

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -12,7 +12,7 @@ class UserSessionsController < ApplicationController
       redirect_back_or_to(:users, notice: 'Login successful')
     else
       flash.now[:alert] = 'Login failed'
-      render action: 'new'
+      render action: "new"
     end
   end
 
